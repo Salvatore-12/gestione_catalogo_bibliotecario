70405de9c7b889cc4aac48f0e0085040ea9ab9ec
Salvatore Assennato <137400679+Salvatore-12@users.noreply.github.com>
2023-12-15 16:58:29 +0100

    update userDAO

--- src/main/java/SalvatoreAssennato/Application.java
+++ src/main/java/SalvatoreAssennato/Application.java
@@ -2,10 +2,8 @@ package SalvatoreAssennato;
 
 import SalvatoreAssennato.DAO.BooksDAO;
 import SalvatoreAssennato.DAO.MagazinesDAO;
-import SalvatoreAssennato.Entities.Books;
-import SalvatoreAssennato.Entities.Geners;
-import SalvatoreAssennato.Entities.Magazines;
-import SalvatoreAssennato.Entities.Periodicity;
+import SalvatoreAssennato.DAO.UserDAO;
+import SalvatoreAssennato.Entities.*;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -19,11 +17,14 @@ public class Application {
         EntityManager em = emf.createEntityManager();
         BooksDAO bd = new BooksDAO(em);
         MagazinesDAO md=new MagazinesDAO(em);
+        UserDAO ud=new UserDAO(em);
 
         Books il_Signore_Degli_Anelli=new Books("le due torri", LocalDate.of(2004,2,4),700,"J. R. R. Tolkien", Geners.FANTASY);
         //bd.save(il_Signore_Degli_Anelli);
         Magazines rivistaHorror=new Magazines("the dark side",LocalDate.of(1991,1,12),200,Periodicity.SETTIMANALE);
         //md.save(rivistaHorror);
+        User Luca=new User("Luca","Bianchi",LocalDate.of(2002,8,24));
+        ud.save(Luca);
 
     }
 }
