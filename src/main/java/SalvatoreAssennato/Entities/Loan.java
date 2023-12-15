56c3efb698556d721a0ceb039c3b11170f55c6eb
Salvatore Assennato <137400679+Salvatore-12@users.noreply.github.com>
2023-12-15 16:40:06 +0100

    update create Publiction,User,Loan

---
+++ src/main/java/SalvatoreAssennato/Entities/Loan.java
@@ -0,0 +1,93 @@
+package SalvatoreAssennato.Entities;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name="prestiti")
+public class Loan {
+    @Id
+    @GeneratedValue
+    @JoinColumn(name="loan_id")
+    private long Id;
+    @ManyToOne
+    @JoinColumn(name="user_id",nullable = false)
+    private User user;
+    @ManyToOne
+    @JoinColumn(name="isbn_code",nullable = false)
+    private Publication publication;
+    @Column(name = "start_date",nullable = false)
+    private LocalDate startDate;
+    @Column(name = "due_date")
+    private LocalDate expectedDate;
+    @Column(name = "return_date")
+    private LocalDate returnDate;
+
+    public Loan(){
+
+    }
+
+    public Loan(User user, Publication publication, LocalDate startDate, LocalDate expectedDate, LocalDate returnDate) {
+        this.user = user;
+        this.publication = publication;
+        this.startDate = startDate;
+        this.expectedDate = expectedDate;
+        this.returnDate = returnDate;
+    }
+
+    public long getId() {
+        return Id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Publication getPublication() {
+        return publication;
+    }
+
+    public void setPublication(Publication publication) {
+        this.publication = publication;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getExpectedDate() {
+        return expectedDate;
+    }
+
+    public void setExpectedDate(LocalDate expectedDate) {
+        this.expectedDate = expectedDate;
+    }
+
+    public LocalDate getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(LocalDate returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Loan{" +
+                "Id=" + Id +
+                ", user=" + user +
+                ", publication=" + publication +
+                ", startDate=" + startDate +
+                ", expectedDate=" + expectedDate +
+                ", returnDate=" + returnDate +
+                '}';
+    }
+}
--- src/main/java/SalvatoreAssennato/Entities/User.java
+++ src/main/java/SalvatoreAssennato/Entities/User.java
@@ -2,6 +2,7 @@ package SalvatoreAssennato.Entities;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.List;
 
 @Entity
 @Table(name="utenti")
@@ -14,4 +15,55 @@ public class User {
     @GeneratedValue
     protected long numberCard_id;
 
+    @OneToMany(mappedBy = "user",cascade = CascadeType.REMOVE)
+    private List<Loan> listOfLoans;
+
+    public User(){
+
+    }
+
+    public User(String name, String surname, LocalDate dateOfBirth) {
+        this.name = name;
+        this.surname = surname;
+        this.dateOfBirth = dateOfBirth;
+
+    }
+
+    public long getNumberCard_id() {
+        return numberCard_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", dateOfBirth=" + dateOfBirth +
+                ", numberCard_id=" + numberCard_id +
+                '}';
+    }
 }
